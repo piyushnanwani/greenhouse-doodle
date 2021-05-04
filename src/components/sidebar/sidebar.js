57f13d6059599fc168f8683d0cda7d3e542bef51
piyush.nanwani <pnanwani61@gmail.com>
2021-05-04 18:46:27 +0530

    bug fixed: already selected countries were still appearing in drop down after reload, extra comma in locationStr

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Graph, CountrySelect, ParameterSelect } from './index';
 import { SelectYear, ErrorBoundary } from '../common';
-import {updateBrowserUrl} from '../../utils/update-browser-url';
+import { updateBrowserUrl } from '../../utils/update-browser-url';
 
 export default function Sidebar({
   CLEANED_DATA,
@@ -14,32 +14,31 @@ export default function Sidebar({
   parameter,
   setParameter,
   countryList,
-  setCountryList
+  setCountryList,
 }) {
-
-  
   const [country, setCountry] = useState('Add Location');
 
   let countryListStr = String(countryList);
-  
-  updateBrowserUrl(
-    start,
-    end,
-    parameter,
-    countryListStr
-  );
+
+  updateBrowserUrl(start, end, parameter, countryListStr);
 
   useEffect(() => {
     let countryListStr = localStorage.getItem('countryList'); // returns a string by default
-    let startStr = localStorage.getItem('start'); 
-    let endStr = localStorage.getItem('end'); 
+    let startStr = localStorage.getItem('start');
+    let endStr = localStorage.getItem('end');
     let parameterStr = localStorage.getItem('parameter');
 
-
     if (countryList && countryListStr && countryListStr !== '') {
+      // removing garbage values from countryListStr
+      while (countryListStr[0] === ',') {
+        countryListStr = countryListStr.slice(1, countryListStr.length); 
+      }
       let countryListArr = countryListStr.split(',');
       setCountry(countryListArr[0]);
       setCountryList(countryListArr);
+      // removing already selected countries from country names
+      let tmp = countryNames.filter(ele => countryListArr.indexOf(ele) === -1);
+      setCountryNames(tmp);
     }
     if (startStr) setStart(startStr);
     if (endStr) setEnd(endStr);
@@ -57,6 +56,7 @@ export default function Sidebar({
     setStart(1990);
     setEnd(2014);
   }
+
   return (
     <div className="sidebar">
       <div className="dropdowns">
