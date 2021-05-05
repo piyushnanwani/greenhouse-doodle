eeaac5260ef3c3141f71f7a32ba56d45bdff4be7
piyush.nanwani <pnanwani61@gmail.com>
2021-05-05 15:22:33 +0530

    fixed delete tooltip bug, legend position

--- src/components/map/legend.js
+++ src/components/map/legend.js
@@ -1,6 +1,15 @@
 import React from 'react';
 const Legend = props => {
-  const emmisionsRange = [1000, 3000, 5000, 10000, 25000, 50000, 100000, 200000];
+  const emmisionsRange = [
+    1000,
+    3000,
+    5000,
+    10000,
+    25000,
+    50000,
+    100000,
+    200000,
+  ];
   const renderLegendKeys = (colors, i) => {
     return (
       <div key={i}>
@@ -13,18 +22,16 @@ const Legend = props => {
   };
 
   return (
-    <>
-      <div className="legend-container">
-        <div className="legend-header">
-          <p>{props.parameter}</p>
-          <p style={{ fontSize: 12 }}>( In Kilo Tons )</p>
-          <p>
-            {props.start}-{props.end} 
-          </p>
-        </div>
-        {props.colors.map(renderLegendKeys)}
+    <div className="legend-container">
+      <div className="legend-header">
+        <p>{props.parameter}</p>
+        <p style={{ fontSize: 12 }}>( In Kilo Tons )</p>
+        <p>
+          {props.start}-{props.end}
+        </p>
       </div>
-    </>
+      {props.colors.map(renderLegendKeys)}
+    </div>
   );
 };
 
